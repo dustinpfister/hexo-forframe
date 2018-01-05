63bd3703615906b7b2d98bee9f69a3d5d0d3580d
dustinpfister <dustin.pfister@gmail.com>
2018-01-05 11:43:55 -0500

    added lodash, and reading source folder

--- index.js
+++ index.js
@@ -1,22 +1,67 @@
 
-let fs = require('fs');
+let fs = require('fs'),
+path = require('path'),
+_ = require('lodash');
 
 // generate the index.html file at /forframe/index
 hexo.extend.generator.register('forframe_index', function (locals) {
 
-    return {
+    console.log(hexo);
 
-        path: 'forframe/index.html',
-        data: locals,
-        layout: 'forframe_index'
+    return new Promise(function (resolve, reject) {
 
-    };
+        fs.readdir(path.join(hexo.source_dir,'_forframe'), function (e, files) {
+
+            if (e) {
+
+                reject(e);
+
+            }
+
+            resolve(files);
+
+        });
+
+    }).then(function (files) {
+
+        return {
+
+            path: 'forframe/index.html',
+            data: _.merge(locals, {
+                data: {
+
+                    files: files,
+                    message: 'looks good'
+
+                }
+            }),
+            layout: 'forframe_index'
+
+        };
+
+    }).catch (function (e) {
+
+        return {
+
+            path: 'forframe/index.html',
+            data: _.merge(locals, {
+                data: {
+
+                    message: e.message
+
+                }
+            }),
+            layout: 'forframe_index'
+
+        };
+
+    });
 
 });
 
 // make a list of currently loaded animations
 hexo.extend.helper.register('ff_list', function () {
 
-    return '<p>I am helping.</p>'
+    return 'yeah I am just a helper.'
 
 });
