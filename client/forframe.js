50579bcd338b454ed28d442a55b6c14f36d6b7c9
dustinpfister <dustin.pfister@gmail.com>
2018-01-10 13:53:53 -0500

    click or touch to step frame

--- client/forframe.js
+++ client/forframe.js
@@ -2,6 +2,7 @@ var forFrame = (function () {
 
     var container = document.getElementById('ff') || document.body;
 
+    // the api for the init methods of a ff object
     var initAPI = {
 
         add: function (type, id, draw) {
@@ -36,6 +37,18 @@ var forFrame = (function () {
 
         }
 
+    },
+
+    tick = function (ff) {
+
+        // step fram index
+        ff.i += 1;
+        if (ff.i >= ff.maxFrame) {
+
+            ff.i = 0;
+
+        }
+
     };
 
     return function (ff) {
@@ -48,6 +61,11 @@ var forFrame = (function () {
         ff.obj = [];
         ff.width = ff.width || 320;
         ff.height = ff.height || 240;
+        ff.name = ff.name || 'untitled';
+
+        // these should be given, but default to noop anyway
+        ff.init = ff.init || function () {};
+        ff.forFrame = ff.forFrame || function () {};
 
         // using phaser
         ff.game = new Phaser.Game(ff.width, ff.height, Phaser.AUTO, container, {
@@ -55,6 +73,13 @@ var forFrame = (function () {
                 create: function () {
 
                     ff.init.call(_.merge(ff, initAPI));
+
+                    ff.game.input.onDown.add(function () {
+
+                        console.log('yeah');
+                        tick(ff);
+
+                    }, this)
 
                 },
 
@@ -76,13 +101,7 @@ var forFrame = (function () {
 
                     }
 
-                    // step fram index
-                    ff.i += 1;
-                    if (ff.i >= ff.maxFrame) {
-
-                        ff.i = 0;
-
-                    }
+                    //tick(ff);
 
                 }
 
